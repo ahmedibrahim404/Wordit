100d583b605d6747a8e02be56eace01b0f626fca
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-02-12 12:35:40 +0200

    feat(socket): Integrate socket.io to server & testing using Postman

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const app = express();
 const dotenv = require('dotenv').config({path:__dirname+'/.env'});
 const PORT = require('./config').PORT || 3000;
 
-app.listen(PORT, () => {
+const { Server } = require("socket.io");
+
+var server = app.listen(PORT, () => {
     console.log("Server is running over port " + PORT);
+});
+
+const io = new Server(server);
+
+
+io.on('connection', (socket) => {
+    console.log(socket.id + " User connected!");
+    socket.on('disconnect', () => {
+        console.log(socket.id +  " Disconnected :(");
+    });
 });
