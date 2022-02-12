82c3806c809298c67a0e1f78eee13ebd6a2d4459
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-02-12 14:01:08 +0200

    feat(create-contest): Integrate create contest with socket

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config({path:__dirname+'/.env'});
-const PORT = require('./config').PORT || 3000;
+const config = require('./config');
+const PORT = config.PORT || 3000;
+const REQ_NUM = config.REQ_NUM || 3; 
 
 const { Server } = require("socket.io");
 
@@ -11,10 +13,32 @@ var server = app.listen(PORT, () => {
 
 const io = new Server(server);
 
+let currentUsersQueue = new Set();
+
+const User = require('./models/user.model');
+const Contest = require('./models/contest.model');
+const createContest = require('./functionalities/createContest');
+
 
 io.on('connection', (socket) => {
     console.log(socket.id + " User connected!");
+    let userID = socket.id;
+    let user = new User(userID);
+
+    socket.on('enter', () => {
+        currentUsersQueue.add(user);
+        if(currentUsersQueue.size == REQ_NUM){
+            let contest = createContest(new Set(currentUsersQueue));
+            currentUsersQueue.clear();
+            io.emit('start-contest');
+            contest.start();
+        }
+    });
+
     socket.on('disconnect', () => {
+        currentUsersQueue.delete(user);
         console.log(socket.id +  " Disconnected :(");
     });
+
 });
+
