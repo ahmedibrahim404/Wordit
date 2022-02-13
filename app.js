f3768c2b83a32c58efadd26a22dadd456d35e3a0
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-02-13 12:55:26 +0200

    feat(): Integrate contest system and timing with socket

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv').config({path:__dirname+'/.env'});
 const config = require('./config');
 const PORT = config.PORT || 3000;
 const REQ_NUM = config.REQ_NUM || 3; 
+const CONTEST_TIME = config.CONTEST_TIME || 10000; // 10 seconds
 
 const { Server } = require("socket.io");
 
@@ -27,15 +28,42 @@ io.on('connection', (socket) => {
     let userID = socket.id;
     let user = new User(userID);
 
-    socket.on('enter', () => {
+    socket.on('enter-queue', () => {
+        if(user.isInContest()) return;
         currentUsersQueue.add(user);
         if(currentUsersQueue.size == REQ_NUM){
             let contest = createContest(new Set(currentUsersQueue), words);
+            
+            let contestants = [...currentUsersQueue];
             currentUsersQueue.clear();
-            io.emit('start-contest');
-            contest.start();
-        }
 
+            for(let contestant of contestants){
+                io.to(contestant.getContestantID()).emit('start-contest', {
+                    
+                });
+            }
+
+            contest.start();
+
+            setTimeout(() => {
+                contest.endContest();
+                for(let contestant of contestants){
+                    contestant.leaveContest();
+                    io.to(contestant.getContestantID()).emit('end-contest', {
+
+                    });
+                }
+            }, CONTEST_TIME);    
+
+        }
+    });
+
+
+    socket.on('enter-word', ({wordEntered, wordIndex}) => {
+        if(!user.isInContest() || !user.canTryWord(wordIndex)) return;
+        let guessAnswer = user.guessWord(wordIndex, wordEntered, words);
+        if(guessAnswer.error) return;
+        io.to(userID).emit('enter-word-response', guessAnswer);
     });
 
     socket.on('disconnect', () => {
