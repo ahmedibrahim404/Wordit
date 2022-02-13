e008ca4cf23b62b8677a4cf8fe3efd90348ce3a1
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-02-13 12:52:59 +0200

    Add Contest time to ENV vars

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env["PORT"],
     REQ_NUM: process.env["REQ_NUM"],
     WORDS_PER_CONTEST: process.env["WORDS_PER_CONTEST"],
-    MAX_NUMBER_OF_TRIALS: process.env["MAX_NUMBER_OF_TRIALS"]
+    MAX_NUMBER_OF_TRIALS: process.env["MAX_NUMBER_OF_TRIALS"],
+    CONTEST_TIME: process.env["CONTEST_TIME"]
 };
