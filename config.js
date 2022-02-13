4e2edd432709dc472c29e1201fcb3c23bf41ebea
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-02-13 12:15:53 +0200

    Added Max number of trials per word

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env["PORT"],
     REQ_NUM: process.env["REQ_NUM"],
-    WORDS_PER_CONTEST: process.env["WORDS_PER_CONTEST"]
+    WORDS_PER_CONTEST: process.env["WORDS_PER_CONTEST"],
+    MAX_NUMBER_OF_TRIALS: process.env["MAX_NUMBER_OF_TRIALS"]
 };
