fcced7fff7675e1cc5a9e4e2bb9dcdeb26c56082
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-02-14 05:25:14 +0200

    feat(routing-system): add routes of components

--- frontend/wordit/src/App.js
+++ frontend/wordit/src/App.js
@@ -5,12 +5,48 @@ import GuessWordHome from './components/GuessWord/Home/GuessWordHome';
 import GuessWordPanelRow from './components/GuessWord/Objects/GuessWordPanelRow/GuessWordPanelRow';
 import Grid from '@mui/material/Grid';
 import GuessWordPlay from './components/GuessWord/GuessWordPlay/GuessWordPlay';
+import { createTheme } from '@mui/material/styles';
+import MuiThemeProvider from '@mui/material/styles/ThemeProvider';
+import backgroundH from "./vid.gif";
+import Navbar from './components/main-components/Navbar/Navbar';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CreateContestForm from './components/GuessWord/Objects/CreateContestForm/CreateContestForm';
+import JoinContestForm from './components/GuessWord/Objects/JoinContestForm/JoinContestForm';
+
+const theme = createTheme({
+  palette: {
+    primary:{
+      main:'#2E2E2E',
+    },
+    secondary: {
+      main: '#E33E7F'
+    }
+  }
+});
+
 
 function App() {
   return (
-    <div>
-      <GuessWordPlay />
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <Navbar />
+      <div
+        style={{
+          //backgroundImage: "url(" + backgroundH + ")",
+          backgroundSize: "cover",
+          height: "100vh",
+        }}
+      >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/guessword" element={<GuessWordHome />} />
+          <Route path="/guessword/play" element={<GuessWordPlay />} />
+          <Route path="/guessword/createcontest" element={<CreateContestForm />} />
+          <Route path="/guessword/joincontest" element={<JoinContestForm />} />
+        </Routes>
+      </BrowserRouter>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
