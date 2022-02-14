503b52a7dae8f990776d0c0f659cbdc637cd3ac8
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-02-14 03:23:49 +0200

    feat(navbar): Add logo to navbar

--- frontend/wordit/src/components/main-components/Navbar/Navbar.js
+++ frontend/wordit/src/components/main-components/Navbar/Navbar.js
@@ -14,7 +14,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-
+import logoImg from '../../../scrabble.png';
 import { games } from '../../../config';
 
 const Navbar = () => {
@@ -22,20 +22,32 @@ const Navbar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+  const style = {
+    textShadow: `1px 1px 0 #000,
+    -1px 1px 0 #000,
+    1px -1px 0 #000,
+    -1px -1px 0 #000,
+    0px 1px 0 #000,
+    0px -1px 0 #000,
+    -1px 0px 0 #000,
+    1px 0px 0 #000,
+    2px 2px 0 #000,
+    -2px 2px 0 #000,
+    2px -2px 0 #000,
+    -2px -2px 0 #000,
+    0px 2px 0 #000,
+    0px -2px 0 #000,
+    -2px 0px 0 #000,
+    2px 0px 0 #000,
+    1px 2px 0 #000,
+    -1px 2px 0 #000,
+    1px -2px 0 #000,
+    -1px -2px 0 #000,
+    2px 1px 0 #000,
+    -2px 1px 0 #000,
+    2px -1px 0 #000,
+    -2px -1px 0 #000`,
+  }
 
   return (
     <AppBar position="static">
@@ -45,18 +57,21 @@ const Navbar = () => {
             variant="h6"
             noWrap
             component="div"
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' },justifyContent: 'center', alignItems: 'center' }}
           >
-            LOGO
+            <img src={logoImg} style={{height:"50px", width:"50px", padding:"10px"}} />
+            <div style={{color:"#A4A4A4", textShadow:style.textShadow}}>Wordit</div>
           </Typography>
+
 
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } , justifyContent: 'center', alignItems: 'center'}}
           >
-            LOGO
+            <img src={logoImg} style={{height:"50px", width:"50px", padding:"10px"}} />
+            <div style={{color:"#A4A4A4", textShadow:style.textShadow}}>Wordit</div>
           </Typography>
         </Toolbar>
       </Container>
