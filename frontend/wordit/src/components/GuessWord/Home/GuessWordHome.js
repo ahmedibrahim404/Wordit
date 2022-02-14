7f063384e8b03d75d5f37fde569cbdc04152a0e9
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-02-14 05:23:54 +0200

    feat(navigation-buttons): Implement navigation func. for create/enter contest buttons

--- frontend/wordit/src/components/GuessWord/Home/GuessWordHome.js
+++ frontend/wordit/src/components/GuessWord/Home/GuessWordHome.js
@@ -5,13 +5,20 @@ import Navbar from '../../main-components/Navbar/Navbar';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
+import { Fab } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Box } from '@mui/system';
+import { Link } from 'react-router-dom';
+import GuessWordPanel from '../GuessWordPanel/GuessWordPanel';
 
 const options = [
   {
-    name:'Create Game'
+    name:'Create Contest',
+    link:'/guessword/createcontest/'
   },
   {
-    name:'Enter Game'
+    name:'Join Contest',
+    link:'/guessword/joincontest/'
   }
 ]
 
@@ -20,10 +27,25 @@ const GuessWordHome = () => (
       <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center">
         {
           options.map((option) => {
-            console.log(option);
-            return <Button variant="contained" style={{minWidth:'70%', minHeight:'50px', margin:'25px'}}>{option.name}</Button>
+            return (
+              <Link to={`${option.link}`} style={{ width:'100%', textDecoration: 'none', color:'white' }}>
+                <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center">
+                  <Button variant="contained" style={{minWidth:'70%', minHeight:'50px', margin:'25px'}}>
+                    {option.name}
+                  </Button>
+                </Grid>
+              </Link>
+            );
           })
         }
+
+
+        <Link to={"/"}> 
+          <Box sx={{ '& > :not(style)': { m: 1 } }} style={{position:"fixed", left:15, bottom:15}}>
+            <Fab variant="extended"><ArrowBackIcon sx={{ mr: 1 }}  /></Fab>
+          </Box>
+        </Link>
+
       </Grid>
   </div>
 );
