543f1773669d1115cd939d77cc027afba813b03f
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-02-14 07:30:09 +0200

    feat(socket-client): Integrate socketio to clientside

--- frontend/wordit/src/App.js
+++ frontend/wordit/src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
 import Home from './components/Home/Home';
 import GuessWordHome from './components/GuessWord/Home/GuessWordHome';
-import GuessWordPanelRow from './components/GuessWord/Objects/GuessWordPanelRow/GuessWordPanelRow';
-import Grid from '@mui/material/Grid';
 import GuessWordPlay from './components/GuessWord/GuessWordPlay/GuessWordPlay';
-import { createTheme } from '@mui/material/styles';
-import MuiThemeProvider from '@mui/material/styles/ThemeProvider';
-import backgroundH from "./vid.gif";
 import Navbar from './components/main-components/Navbar/Navbar';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CreateContestForm from './components/GuessWord/Objects/CreateContestForm/CreateContestForm';
 import JoinContestForm from './components/GuessWord/Objects/JoinContestForm/JoinContestForm';
+
+// import backgroundH from "./vid.gif";
+
+import { createTheme } from '@mui/material/styles';
+import MuiThemeProvider from '@mui/material/styles/ThemeProvider';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {SocketContext, socket} from './utilities/socket';
 
 const theme = createTheme({
   palette: {
@@ -27,26 +29,28 @@ const theme = createTheme({
 
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
-      <Navbar />
-      <div
-        style={{
-          //backgroundImage: "url(" + backgroundH + ")",
-          backgroundSize: "cover",
-          height: "100vh",
-        }}
-      >
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/guessword" element={<GuessWordHome />} />
-          <Route path="/guessword/play" element={<GuessWordPlay />} />
-          <Route path="/guessword/createcontest" element={<CreateContestForm />} />
-          <Route path="/guessword/joincontest" element={<JoinContestForm />} />
-        </Routes>
-      </BrowserRouter>
-      </div>
-    </MuiThemeProvider>
+    <SocketContext.Provider value={socket}>
+      <MuiThemeProvider theme={theme}>
+        <Navbar />
+        <div
+          style={{
+            //backgroundImage: "url(" + backgroundH + ")",
+            backgroundSize: "cover",
+            height: "100vh",
+          }}
+        >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/guessword" element={<GuessWordHome />} />
+            <Route path="/guessword/play" element={<GuessWordPlay />} />
+            <Route path="/guessword/createcontest" element={<CreateContestForm />} />
+            <Route path="/guessword/joincontest" element={<JoinContestForm />} />
+          </Routes>
+        </BrowserRouter>
+        </div>
+      </MuiThemeProvider>
+    </SocketContext.Provider>
   );
 }
 
