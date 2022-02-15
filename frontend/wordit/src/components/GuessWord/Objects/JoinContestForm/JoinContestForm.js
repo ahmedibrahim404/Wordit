c60785f7d2c0a92a07e2f3216cad564034689908
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-02-15 03:50:47 +0200

    feat(create-contest): Attach join contest form to backend using sockets

--- frontend/wordit/src/components/GuessWord/Objects/JoinContestForm/JoinContestForm.js
+++ frontend/wordit/src/components/GuessWord/Objects/JoinContestForm/JoinContestForm.js
@@ -5,28 +5,89 @@ import { Button, Fab, Grid, TextField } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { Box } from '@mui/system';
 import { Link } from 'react-router-dom';
+import { socket } from '../../../../utilities/socket';
+import { Navigator } from '../Navigator/Navigator';
 
-const JoinContestForm = () => (
-  <div style={{display: 'flex',alignItems: 'center',justifyContent: 'center', margin:'10px'}} data-testid="JoinContestForm">
+class JoinContestForm extends React.Component {
 
-    <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center">
-        <h2>Join Word Guess Contest</h2>
-        <TextField id="username" label="Username" variant="standard" style={{minWidth:"50%"}} />
-        <TextField id="contest-code" label="Contest ID(leave empty if none)" variant="standard" style={{minWidth:"50%"}}  />
-        <Button variant="contained" style={{minWidth:'50%', minHeight:'40px', margin:'25px'}}>Join Contest</Button>    
-    </Grid>
+  constructor(props){
+    super(props);
+    this.io = socket;
+    this.state = {
+      username:'',
+      contestCode:''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.joinContest = this.joinContest.bind(this);
+    this.joinGlobalContest = this.joinGlobalContest.bind(this);
+    this.joinPrivateContest = this.joinPrivateContest.bind(this);
+    
+  }
+
+  componentDidMount(){
+    this.io.on('enter-queue-wait', () => {
+      this.props.myNavigation("/guessword/play/");
+    });
+  }
+
+  joinContest(){
+    let username = this.state.username;
+    if(username == '') return;
+    let contestCode = this.state.contestCode;
+    if(contestCode == '') return this.joinGlobalContest();
+    return this.joinPrivateContest();
+  }
+
+  joinGlobalContest(){
+    let username = this.state.username;
+    this.io.emit('enter-queue', {
+      username
+    });
+  }
+
+  joinPrivateContest(){
+    let username = this.state.username;
+    let contestCode = this.state.contestCode;
+    this.io.emit('enter-private-queue', {
+      username, contestCode
+    });
+  }
+
+  handleChange(e){
+    let targetID = e.target.id;
+    let targetVal = e.target.value;
+    this.setState({
+      ...this.state,
+      [targetID]: targetVal
+    });    
+  }
+
+  
+  render(){
+    return (
+      <div style={{display: 'flex',alignItems: 'center',justifyContent: 'center'}} data-testid="JoinContestForm">
+
+        <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center">
+            <h2>Join Word Guess Contest</h2>
+            <TextField id="username" onChange={this.handleChange} value={this.state.username} label="Username" variant="standard" style={{minWidth:"50%"}} />
+            <TextField id="contestCode" onChange={this.handleChange} value={this.state.contestCode} label="Contest ID(leave empty if none)" variant="standard" style={{minWidth:"50%"}}  />
+            <Button variant="contained" onClick={this.joinContest} style={{backgroundColor:'#181818', fontWeight:'bold' , minWidth:'50%', minHeight:'40px', margin:'25px'}}>Join Contest</Button>    
+        </Grid>
 
 
-    <Link to={"/guessword"}> 
-      <Box sx={{ '& > :not(style)': { m: 1 } }} style={{position:"fixed", left:15, bottom:15}}>
-        <Fab variant="extended"><ArrowBackIcon sx={{ mr: 1 }}  /></Fab>
-      </Box>
-    </Link>
-  </div>
-);
+        <Link to={"/guessword"}> 
+          <Box sx={{ '& > :not(style)': { m: 1 } }} style={{position:"fixed", left:15, bottom:15}}>
+            <Fab variant="extended"><ArrowBackIcon sx={{ mr: 1 }}  /></Fab>
+          </Box>
+        </Link>
+      </div>
+    );
+  }
+}
 
 JoinContestForm.propTypes = {};
 
 JoinContestForm.defaultProps = {};
 
-export default JoinContestForm;
+export default Navigator(JoinContestForm);
