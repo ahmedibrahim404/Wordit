6c7f39a61b4bcf418850815d74cfbdb8ee563552
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-02-15 04:51:03 +0200

    feat(opponent-panels): make other players results appear to all

--- frontend/wordit/src/components/GuessWord/GuessWordPlay/GuessWordPlay.js
+++ frontend/wordit/src/components/GuessWord/GuessWordPlay/GuessWordPlay.js
@@ -7,6 +7,7 @@ import Scoreboard from '../Objects/Scoreboard/Scoreboard';
 import MuiGrid from '@mui/material/Grid';
 import Divider from '@mui/material/Divider';
 import { styled } from '@mui/material/styles';
+import { socket } from '../../../utilities/socket';
 
 const Grid = styled(MuiGrid)(({ theme }) => ({
   width: '100%',
@@ -17,31 +18,55 @@ const Grid = styled(MuiGrid)(({ theme }) => ({
 }));
 
 
-const GuessWordPlay = () => (
-  <div className={styles.GuessWordPlay} data-testid="GuessWordPlay">
+class GuessWordPlay extends React.Component {
 
-  <Scoreboard />
+  constructor(props){
+    super(props);
+    this.io = socket;
 
-  <Grid container spacing={2}>
-    <Grid item xs={8}>
+    this.state = {
+      users:[]
+    }
+  }
 
-      <GuessWordPanel mainPlayer={true}/>
 
+  componentDidMount(){
+    this.io.on('players-joined', ({users}) => {
+      this.setState({
+        users
+      });
+    });
+  }
+
+  render(){
+    return (
+    <div className={styles.GuessWordPlay} data-testid="GuessWordPlay">
+
+    <Scoreboard />
+
+    <Grid container spacing={2}>
+      <Grid item xs={8}>
+
+        <GuessWordPanel mainPlayer={true} ownerID={localStorage.getItem('player-id')}/>
+
+      </Grid>
+      <Divider orientation="vertical" flexItem style={{marginTop:"50px"}} />
+
+      <Grid item xs={3}>
+
+      {
+        this.state.users.map((user) => (user != localStorage.getItem('player-id') ? <GuessWordPanel ownerID={user} mainPlayer={false}/> : null))
+      }
+      
+
+      </Grid>
     </Grid>
-    <Divider orientation="vertical" flexItem style={{marginTop:"50px"}} />
-
-    <Grid item xs={3}>
-
-    <GuessWordPanel mainPlayer={false}/>
-    <GuessWordPanel mainPlayer={false}/>
-    <GuessWordPanel mainPlayer={false}/>
-
-    </Grid>
-  </Grid>
 
 
-  </div>
-);
+    </div>
+    );
+  }
+};
 
 GuessWordPlay.propTypes = {};
 
