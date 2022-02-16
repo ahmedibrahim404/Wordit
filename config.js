dfe50b600a04d609a1ce00bba20a6fe4d7d40a05
ahmedibrahim404 <ahmedie20022011@gmail.com>
2022-02-16 03:48:00 +0200

    updated config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     PORT: process.env["PORT"],
-    REQ_NUM: process.env["REQ_NUM"],
-    WORDS_PER_CONTEST: process.env["WORDS_PER_CONTEST"],
-    MAX_NUMBER_OF_TRIALS: process.env["MAX_NUMBER_OF_TRIALS"],
-    CONTEST_TIME: process.env["CONTEST_TIME"],
-    PENALITY_PER_WRONG: process.env["PENALITY_PER_WRONG"],
+    REQ_NUM: process.env["REQ_NUM"] || 2,
+    WORDS_PER_CONTEST: process.env["WORDS_PER_CONTEST"] || 5,
+    MAX_NUMBER_OF_TRIALS: process.env["MAX_NUMBER_OF_TRIALS"] || 3,
+    CONTEST_TIME: process.env["CONTEST_TIME"] || 60000,
+    PENALITY_PER_WRONG: process.env["PENALITY_PER_WRONG"] || 10,
 };
